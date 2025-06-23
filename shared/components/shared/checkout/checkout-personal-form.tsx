cc6697e7c6fda621964ec7eeda2261b26df62913
Даниил <Gruzdev_OnlyWork@vk.com>
2025-06-24 00:20:59 +0300

    PersonalData

--- shared/components/shared/checkout/checkout-personal-form.tsx
+++ shared/components/shared/checkout/checkout-personal-form.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { WhiteBlock } from '../white-block';
 import { FormInput } from '../form';
+import { Checkbox } from '../../ui';
 
 
 interface Props {
@@ -13,6 +14,8 @@ export const CheckoutPersonalForm: React.FC<Props> = ({ className }) => {
     
     <WhiteBlock title="2. Персональные данные" className={className}>
       <div className="grid grid-cols-2 gap-5">
+        <Checkbox required></Checkbox>
+        <label htmlFor="">Я ознакомлен и согласен с <a href='https://btgp.ru/images/new_site/test_dpl/PersonalData/PersonalData.pdf'>политикой обработки персональных данных</a></label>
         <FormInput name="firstName" className="text-base" placeholder="Имя" />
         <FormInput name="lastName" className="text-base" placeholder="Фамилия" />
         <FormInput name="email" className="text-base" placeholder="E-Mail" />
